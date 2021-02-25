97a58fb5ab9c6bb8ffdc6fd9e9c2a792224807fe
huylinh620 <huylinh620@gmal.com>
2021-02-25 18:33:59 +0700

    Update frontend boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,33 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const postcssCustomMedia = require('postcss-custom-media');
+const postcssMixin = require('postcss-mixins');
 const postcssImport = require('postcss-import');
 const cssnano = require('cssnano');
 const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
+const image = require('gulp-image');
 
 // Sass Task
 function scssTask(){
-  return src('src/css/style.css', { sourcemaps: false })
+  return src('src/assets/css/style.css', { sourcemaps: false })
     .pipe(sass())
-    .pipe(postcss([/* cssnano(), */ postcssCustomMedia(), postcssImport()]))
+    .pipe(postcss([cssnano(), postcssCustomMedia(), postcssMixin(), postcssImport()]))
     .pipe(dest('dist/css', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
 function jsTask(){
-  return src('src/js/script.js', { sourcemaps: false })
+  return src('src/assets/js/script.js', { sourcemaps: false })
     .pipe(terser())
     .pipe(dest('dist/js', { sourcemaps: '.' }));
+}
+
+// Image task
+function imgTask() {
+  return src('src/assets/image/*')
+  .pipe(image())
+  .pipe(dest('dist/image'));
 }
 
 // Browsersync Tasks
@@ -40,7 +49,7 @@ function browsersyncReload(cb){
 // Watch Task
 function watchTask(){
   watch('*.html', browsersyncReload);
-  watch(['src/css/*.css', 'src/js/*.js'], series(scssTask, jsTask, browsersyncReload));
+  watch(['src/assets/css/*.css', 'src/assets/js/*.js'], series(scssTask, jsTask, browsersyncReload));
 }
 
 // Default Gulp task
@@ -49,4 +58,8 @@ exports.default = series(
   jsTask,
   browsersyncServe,
   watchTask
+);
+
+exports.image = series(
+  imgTask
 );
